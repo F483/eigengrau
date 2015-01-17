4a411c7ad7f747ae398fde8895879a1c798ac0d4
F483 <fabian.barkhau@gmail.com>
2015-01-17 18:22:50 +0100

    fixed sequencer_bg

--- gfx/sequencer_bg.h
+++ gfx/sequencer_bg.h
@@ -5,11 +5,11 @@
 //
 //	sequencer_bg_, 256x256@8, 
 //	+ palette 256 entries, not compressed
-//	+ 160 tiles (t|f|p reduced) not compressed
+//	+ 161 tiles (t|f|p reduced) not compressed
 //	+ regular map (flat), not compressed, 32x32 
-//	Total size: 512 + 10240 + 2048 = 12800
+//	Total size: 512 + 10304 + 2048 = 12864
 //
-//	Time-stamp: 2015-01-13, 15:17:02
+//	Time-stamp: 2015-01-13, 15:47:10
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_SEQUENCER_BG__H
 #define GRIT_SEQUENCER_BG__H
 
-#define sequencer_bg_TilesLen 10240
-extern const unsigned short sequencer_bg_Tiles[5120];
+#define sequencer_bg_TilesLen 10304
+extern const unsigned short sequencer_bg_Tiles[5152];
 
 #define sequencer_bg_MapLen 2048
 extern const unsigned short sequencer_bg_Map[1024];
